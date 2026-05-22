9724071569d650fb9e07c5f1211cdcf1902ceebd
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2026-05-22 13:33:25 +0400

    aws_secrets change

--- app/aws_secrets.py
+++ app/aws_secrets.py
@@ -1,5 +1,8 @@
 import logging
 import os
+import boto3
+import json
+from urllib.parse import quote_plus
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -8,19 +11,19 @@ logger = logging.getLogger(__name__)
 def get_secrets():
 
     try:
-        # client = boto3.client("secretsmanager", region_name="eu-central-1")
-        # response = client.get_secret_value(SecretId="EmbeddingsDatabaseAdminAccess")
+        client = boto3.client("secretsmanager", region_name="eu-central-1")
+        response = client.get_secret_value(SecretId="EmbeddingsDatabaseAdminAccess")
 
-        # secret = json.loads(response["SecretString"])
+        secret = json.loads(response["SecretString"])
 
-        # username = quote_plus(secret["username"])
-        # password = quote_plus(secret["password"])
-        # host = secret["host"]
-        # port = secret["port"]
-        # database = "postgres"
+        username = quote_plus(secret["username"])
+        password = quote_plus(secret["password"])
+        host = secret["host"]
+        port = secret["port"]
+        database = "postgres"
 
-        # return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
-        return os.getenv("DB_URL")
+        return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
+
     except Exception as e:
         logger.error(f"Error getting secrets: {e}")
         return os.getenv("DB_URL")
